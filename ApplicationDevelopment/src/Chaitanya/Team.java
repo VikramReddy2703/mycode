90cb516e13ad73770c8da645d7283ddad24bd22f
mchai <mchai@ChaitanyaMedam>
2023-08-29 19:04:27 +0530

    Developer 2 Commit

---
+++ ApplicationDevelopment/src/Chaitanya/Team.java
@@ -0,0 +1,10 @@
+package Chaitanya;
+
+public class Team {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi welcome to Chaitanya's Page");
+	}
+
+}
